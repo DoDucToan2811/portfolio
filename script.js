2a563068a4cc7257d6a60b8c5350c9ccefe7f7c2
xx28 J4F <10421060@student.vgu.edu.vn>
2024-11-25 14:27:46 +0700

    abc

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ document.getElementById('contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
     alert('Thank you for reaching out!');
   });
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
   
