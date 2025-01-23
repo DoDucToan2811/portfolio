4eaa121fac68202b7ec6a1802e90a59659b288e1
xx28 J4F <10421060@student.vgu.edu.vn>
2025-01-23 16:29:39 +0700

    big

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Welcome to my portfolio!");
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({
+    duration: 1200, // Animation duration in milliseconds
+    once: true, // Animation occurs only once
+  });
+});
