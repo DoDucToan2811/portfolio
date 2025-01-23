234bf8fe9b2689a1252d860a367dbbd62d022b27
xx28 J4F <10421060@student.vgu.edu.vn>
2025-01-23 15:31:47 +0700

    some change

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.getElementById('contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Thank you for reaching out!');
-  });
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
-
-  
+console.log("Welcome to my portfolio!");
